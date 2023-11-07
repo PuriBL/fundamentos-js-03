3e752733cf86232813d5c67d3b63055199ac8178
Puri <purificacion.bastida@gmail.com>
2023-11-07 21:28:27 +0100

    Añadir ejer 4

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Encontrar subcadena:
+
+const sentence = 'Hola, ¿cómo estás?'
+const position = sentence.indexOf('1')
+
+console.log(position)
+
+// el resultado que me sale es -1 y debe ser 2
