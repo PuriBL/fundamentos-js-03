bcccf58d948e6c52b0c1bbf54165609d77420762
Puri <purificacion.bastida@gmail.com>
2023-11-07 22:55:07 +0100

    Añadir ejer 14

--- src/ejercicio-14/index.js
+++ src/ejercicio-14/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+// Verificar número entero:
+
+const num1 = 42
+const num2 = 3.14
+
+console.log(Number.isInteger(num1))  // resultado esperado true
+console.log(Number.isInteger(num2))  // resultado esperado false
+
+
