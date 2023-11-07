9e23613db5ba5867a55c8de5615438ae2a08aba9
Puri <purificacion.bastida@gmail.com>
2023-11-07 23:54:09 +0100

    Añadir ejer 23

--- src/ejercicio-23/index.js
+++ src/ejercicio-23/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+// Copia superficial de subarray. Dado un array de números, utiliza el método 'slice()' 
+// para crear una copia superficial de una porción del array. Luego, imprime el subarray
+// resultante.
+
+const numbers = [1, 2, 3, 4, 5]
+const subArray = numbers.slice(2, 4)
+
+console.log(subArray) // resultado esperado [3, 4]... no lo entiendo 
+
