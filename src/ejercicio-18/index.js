29e0513ea92bd250ae7cdf3309c0ac34468c14c9
Puri <purificacion.bastida@gmail.com>
2023-11-07 23:17:23 +0100

    Añadir ejer 18

--- src/ejercicio-18/index.js
+++ src/ejercicio-18/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Número aleatorio:
+
+const randomNum = Math.floor(Math.random() * 10) + 1
+
+console.log(randomNum)  // resultado esperado núm. aleatorio entre 1 y 10
+
+
