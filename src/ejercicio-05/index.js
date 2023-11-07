0458918d5070cb469a0347d1d14e22c934ce3960
Puri <purificacion.bastida@gmail.com>
2023-11-07 21:34:51 +0100

    Añadir ejer 5

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+// Extraer subcadena:
+
+const originalString = 'Programación en JavaScript'
+const slicedString = originalString.slice(8, 26)
+
+console.log(slicedString) 
