1e28e5eb8d0bb2b27489ab76030cdbd9d924519c
Puri <purificacion.bastida@gmail.com>
2023-11-08 18:07:55 +0100

    Añadir ejercicio 24

--- src/ejercicio-24/index.js
+++ src/ejercicio-24/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Dividir en subcadenas:
+
+const sentence = 'Esta es una oración de ejemplo'
+const words = sentence.split(' ')
+
+console.log(words) // Resultado esperado ['Esta', 'es', 'una', 'oración', 'de', 'ejemplo']
+
+
