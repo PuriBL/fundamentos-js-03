d178b1ec7ab32774f4a28636523199dd2260dd79
Puri <purificacion.bastida@gmail.com>
2023-11-08 18:42:22 +0100

    Añadir ejer 25

--- src/ejercicio-25/index.js
+++ src/ejercicio-25/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Filtrar números pares:
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+const evenNumbers = numbers.filter((num) => num % 2 === 0)
+ console.log(evenNumbers) 
+
+ 
